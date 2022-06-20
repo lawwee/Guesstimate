bfd3e7798894098a748d92aef4c0683baee1e92f
lawwee <lawalmohammed567@gmail.com>
2022-06-20 16:53:41 +0100

    trying chainlink vrf randomness

--- contracts/GameNFT.sol
+++ contracts/GameNFT.sol
@@ -6,13 +6,33 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 
 import { Base64 } from "./libraries/Base64.sol";
 
-contract GameNFT is ERC721URIStorage {
+contract GameNFT is ERC721URIStorage, VRFConsumerBaseV2 {
+    VRFCoordinatorV2Interface COORDINATOR;
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    uint64 s_subscriptionId;
+    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
+    bytes32 s_keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
+    uint32 callbackGasLimit = 40000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords =  1;
+    uint256 private constant ROLL_IN_PROGRESS = 42;
+
+    address s_owner;
+ 
+    event GuessInitiated(uint256 indexed requestId, address indexed guesser);
+    event GuessResult(uint256 indexed requestId, uint256 indexed result);
+
+    mapping(uint256 => address) private s_guesser;
+    mapping(address => uint256) private s_results;
 
     uint256 private seed;
 
@@ -23,95 +43,159 @@ contract GameNFT is ERC721URIStorage {
 
     mapping (address => uint256) public lastWavedAt;
 
-    constructor() ERC721("Game NFT", "GAME") payable {
+    constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) ERC721("Game NFT", "GAME") payable {
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
+        s_owner = msg.sender;
+        s_subscriptionId = subscriptionId;
         console.log("Lawwee is here");
 
-        seed = (block.timestamp + block.difficulty) % 100;
+        // seed = (block.timestamp + block.difficulty) % 100;
     }
 
-    function random(string memory input) internal pure returns (uint256) {
-        return uint256(keccak256(abi.encodePacked(input)));
+    modifier onlyOwner() {
+        require(msg.sender == s_owner);
+        _;
     }
 
-    function randomColor(string memory input) public view returns (uint256) {
-        uint256 rand = random(string(abi.encodePacked("Color", input)));
-        rand = rand % colors.length;
-        return uint256(keccak256(abi.encodePacked(rand)));
+    // function random(string memory input) internal pure returns (uint256) {
+    //     return uint256(keccak256(abi.encodePacked(input)));
+    // }
+
+    // function randomColor(string memory input) public view returns (uint256) {
+    //     uint256 rand = random(string(abi.encodePacked("Color", input)));
+    //     rand = rand % colors.length;
+    //     return uint256(keccak256(abi.encodePacked(rand)));
+    // }
+
+    // function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    //     uint256 rando = randomColor(string(abi.encodePacked("RanDomCOloR", Strings.toString(tokenId))));
+    //     uint256 rand = (block.timestamp + block.difficulty + seed + rando) % 100;
+    //     rand = rand % colors.length;
+    //     return colors[rand];
+    // }
+
+    function guessColor(address guesser) public onlyOwner returns (uint256 requestId) {
+        require(s_results[guesser] == 0, "Already rolled");
+        requestId = COORDINATOR.requestRandomWords(
+        s_keyHash,
+        s_subscriptionId,
+        requestConfirmations,
+        callbackGasLimit,
+        numWords
+       );
+
+        s_guesser[requestId] = guesser;
+        s_results[guesser] = ROLL_IN_PROGRESS;
+        emit GuessInitiated(requestId, guesser);
     }
 
-    function pickRandomColor(uint256 tokenId) public view returns (string memory) {
-        uint256 rando = randomColor(string(abi.encodePacked("RanDomCOloR", Strings.toString(tokenId))));
-        uint256 rand = (block.timestamp + block.difficulty + seed + rando) % 100;
-        rand = rand % colors.length;
-        return colors[rand];
+    function fulfillRandomWords(uint256 requestId , uint256[] memory randomWords) internal override {
+
+        // transform the result to a number between 1 and 20 inclusively
+        uint256 d20Value = (randomWords[0] % 20) + 1;
+        s_results[s_guesser[requestId]] = d20Value;
+        emit GuessResult(requestId, d20Value);
     }
 
     function compareTwoStrings(string memory s1, string memory s2) public pure returns (bool success) {
         return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
     }
 
-    function mintNFT(string memory fword, string memory sword, string memory tword) public {
-        // require(lastWavedAt[msg.sender] + 24 hours < block.timestamp, "Wait a day");
-        // require((lastWavedAt[msg.sender] + 24 hours < block.timestamp) && (lastWavedAt[msg.sender] + 744 hours < block.timestamp), "Wait a month");
-        if ((lastWavedAt[msg.sender] + 24 hours < block.timestamp) && (lastWavedAt[msg.sender] + 744 hours < block.timestamp)) {
-            console.log("Hello");
-        } else if (lastWavedAt[msg.sender] + 24 hours < block.timestamp) {
-            console.log("World");
-        }
-
-        lastWavedAt[msg.sender] = block.timestamp;
-
-        uint256 newItemId = _tokenIds.current();
-
-        string memory someColor = pickRandomColor(newItemId);
-
-        string memory combinedWord = string(abi.encodePacked(fword, sword, tword));
-
-        string memory finalSvg = string(abi.encodePacked(svgPartOne, someColor, svgPartTwo, combinedWord, "</text></svg>"));
-
-        string memory json = Base64.encode(
-            bytes(
-                string(
-                    abi.encodePacked(
-                        '{"name": "',
-                        combinedWord,
-                        '", "description": "A highly acclaimed collection of squares", "image": "data:image/svg+xml;base64',
-                        Base64.encode(bytes(finalSvg)),
-                        '"}'
-                    )
-                )
-            )
-        );
-
-        string memory finalTokenUri = string(
-            abi.encodePacked("data:application/json;base64,", json)
-        );
-
-        console.log(
-            string(
-                abi.encodePacked(
-                    "https://nftpreview.0xdev.codes/?code=",
-                    finalTokenUri
-                )
-            )
-        );
-
-        uint256 prizeAmount = 1 ether;
-
-        bool success = compareTwoStrings(someColor, tword);
-        if (success == true) {
-            require(prizeAmount <= address(this).balance, "You do not have enough ether to send");
-            msg.sender.call{value: prizeAmount}("");
-            console.log("Finally, Lawwee did it");
-        } else {
-            console.log("Still proof that he did it");
-        }
-
-        _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, finalTokenUri);
-        console.log("An NFT w/ ID %s has been minted to %s:", newItemId, msg.sender);
-
-        _tokenIds.increment();
+    function color(address player) public view returns (string memory) {
+        require(s_results[player] != 0, "Player has not guessed");
+        require(s_results[player] != ROLL_IN_PROGRESS, "Guess in progress");
+        return chooseColor(s_results[player]);
     }
+
+    function chooseColor(uint256 id) private pure returns (string memory) {
+        // array storing the list of colors 
+        string[20] memory colorNames = [
+            "Red",
+            "Blue",
+            "Green",
+            "Yellow",
+            "Black",
+            "White",
+            "Magenta",
+            "Indigo",
+            "Cyan",
+            "Violet",
+            "Purple",
+            "Wine",
+            "Brown",
+            "Cream",
+            "Burgundy",
+            "Orange",
+            "Gold",
+            "Silver",
+            "Grey",
+            "Peach"
+        ];
+        return colorNames[id];
+    }
+
+    // function mintNFT(string memory fword, string memory sword, string memory tword) public {
+    //     // require(lastWavedAt[msg.sender] + 24 hours < block.timestamp, "Wait a day");
+    //     // require((lastWavedAt[msg.sender] + 24 hours < block.timestamp) && (lastWavedAt[msg.sender] + 744 hours < block.timestamp), "Wait a month");
+    //     if ((lastWavedAt[msg.sender] + 24 hours < block.timestamp) && (lastWavedAt[msg.sender] + 744 hours < block.timestamp)) {
+    //         console.log("Hello");
+    //     } else if (lastWavedAt[msg.sender] + 24 hours < block.timestamp) {
+    //         console.log("World");
+    //     }
+
+    //     lastWavedAt[msg.sender] = block.timestamp;
+
+    //     uint256 newItemId = _tokenIds.current();
+
+    //     string memory someColor = pickRandomColor(newItemId);
+
+    //     string memory combinedWord = string(abi.encodePacked(fword, sword, tword));
+
+    //     string memory finalSvg = string(abi.encodePacked(svgPartOne, someColor, svgPartTwo, combinedWord, "</text></svg>"));
+
+    //     string memory json = Base64.encode(
+    //         bytes(
+    //             string(
+    //                 abi.encodePacked(
+    //                     '{"name": "',
+    //                     combinedWord,
+    //                     '", "description": "A highly acclaimed collection of squares", "image": "data:image/svg+xml;base64',
+    //                     Base64.encode(bytes(finalSvg)),
+    //                     '"}'
+    //                 )
+    //             )
+    //         )
+    //     );
+
+    //     string memory finalTokenUri = string(
+    //         abi.encodePacked("data:application/json;base64,", json)
+    //     );
+
+    //     console.log(
+    //         string(
+    //             abi.encodePacked(
+    //                 "https://nftpreview.0xdev.codes/?code=",
+    //                 finalTokenUri
+    //             )
+    //         )
+    //     );
+
+    //     uint256 prizeAmount = 1 ether;
+
+    //     bool success = compareTwoStrings(someColor, tword);
+    //     if (success == true) {
+    //         require(prizeAmount <= address(this).balance, "You do not have enough ether to send");
+    //         msg.sender.call{value: prizeAmount}("");
+    //         console.log("Finally, Lawwee did it");
+    //     } else {
+    //         console.log("Still proof that he did it");
+    //     }
+
+    //     _safeMint(msg.sender, newItemId);
+    //     _setTokenURI(newItemId, finalTokenUri);
+    //     console.log("An NFT w/ ID %s has been minted to %s:", newItemId, msg.sender);
+
+    //     _tokenIds.increment();
+    // }
 
 }
--- scripts/run.js
+++ scripts/run.js
@@ -8,29 +8,29 @@ const main = async () => {
 
     console.log("Contract has been deployed to:", gameContract.address); 
 
-    let contractBalance = await hre.ethers.provider.getBalance(gameContract.address)
-    console.log("Contract's address before is:", hre.ethers.utils.formatEther(contractBalance));
+    // let contractBalance = await hre.ethers.provider.getBalance(gameContract.address)
+    // console.log("Contract's address before is:", hre.ethers.utils.formatEther(contractBalance));
 
-    let randomBalance = await hre.ethers.provider.getBalance(randomuser.address);
-    console.log("Owner's address before txn is:", hre.ethers.utils.formatEther(randomBalance));
+    // let randomBalance = await hre.ethers.provider.getBalance(randomuser.address);
+    // console.log("Owner's address before txn is:", hre.ethers.utils.formatEther(randomBalance));
 
-    let txn = await gameContract.connect(randomuser).mintNFT("hello", "lawwee", "green");
-    await txn.wait();
+    // let txn = await gameContract.connect(randomuser).mintNFT("hello", "lawwee", "green");
+    // await txn.wait();
 
-    randomBalance = await hre.ethers.provider.getBalance(randomuser.address);
-    console.log("Owner's address after txn is:", hre.ethers.utils.formatEther(randomBalance));
+    // randomBalance = await hre.ethers.provider.getBalance(randomuser.address);
+    // console.log("Owner's address after txn is:", hre.ethers.utils.formatEther(randomBalance));
 
-    contractBalance = await hre.ethers.provider.getBalance(gameContract.address)
-    console.log("Contract's address after is:", hre.ethers.utils.formatEther(contractBalance));
+    // contractBalance = await hre.ethers.provider.getBalance(gameContract.address)
+    // console.log("Contract's address after is:", hre.ethers.utils.formatEther(contractBalance));
 
-    txn = await gameContract.connect(randomuser).mintNFT("hello", "lawwee", "red");
-    await txn.wait();
+    // txn = await gameContract.connect(randomuser).mintNFT("hello", "lawwee", "red");
+    // await txn.wait();
 
-    randomBalance = await hre.ethers.provider.getBalance(randomuser.address);
-    console.log("Owner's address after txn is:", hre.ethers.utils.formatEther(randomBalance));
+    // randomBalance = await hre.ethers.provider.getBalance(randomuser.address);
+    // console.log("Owner's address after txn is:", hre.ethers.utils.formatEther(randomBalance));
 
-    contractBalance = await hre.ethers.provider.getBalance(gameContract.address)
-    console.log("Contract's address after is:", hre.ethers.utils.formatEther(contractBalance));
+    // contractBalance = await hre.ethers.provider.getBalance(gameContract.address)
+    // console.log("Contract's address after is:", hre.ethers.utils.formatEther(contractBalance));
 
 }
 
